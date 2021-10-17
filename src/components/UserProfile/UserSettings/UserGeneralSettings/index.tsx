f72b84102dd57e860c3354bb8d1da153260befb5
TheCatLady <52870424+TheCatLady@users.noreply.github.com>
2021-10-17 16:10:02 -0400

    feat(login): allow Plex OAuth to be disabled when reqs are met

--- src/components/UserProfile/UserSettings/UserGeneralSettings/index.tsx
+++ src/components/UserProfile/UserSettings/UserGeneralSettings/index.tsx
@@ -181,7 +181,7 @@ const UserGeneralSettings: React.FC = () => {
                   {intl.formatMessage(messages.accounttype)}
                 </label>
                 <div className="mb-1 text-sm font-medium leading-5 text-gray-400 sm:mt-2">
-                  <div className="flex max-w-lg items-center">
+                  <div className="flex items-center max-w-lg">
                     {user?.userType === UserType.PLEX ? (
                       <Badge badgeType="warning">
                         {intl.formatMessage(messages.plexuser)}
@@ -199,7 +199,7 @@ const UserGeneralSettings: React.FC = () => {
                   {intl.formatMessage(messages.role)}
                 </label>
                 <div className="mb-1 text-sm font-medium leading-5 text-gray-400 sm:mt-2">
-                  <div className="flex max-w-lg items-center">
+                  <div className="flex items-center max-w-lg">
                     {user?.id === 1
                       ? intl.formatMessage(messages.owner)
                       : hasPermission(Permission.ADMIN)
@@ -335,13 +335,13 @@ const UserGeneralSettings: React.FC = () => {
                       </label>
                       <div className="form-input-area">
                         <div className="flex flex-col">
-                          <div className="mb-4 flex items-center">
+                          <div className="mb-1.5 flex items-center">
                             <input
                               type="checkbox"
                               checked={movieQuotaEnabled}
                               onChange={() => setMovieQuotaEnabled((s) => !s)}
                             />
-                            <span className="ml-2 text-gray-300">
+                            <span className="ml-2 font-semibold text-gray-300">
                               {intl.formatMessage(messages.enableOverride)}
                             </span>
                           </div>
@@ -375,13 +375,13 @@ const UserGeneralSettings: React.FC = () => {
                       </label>
                       <div className="form-input-area">
                         <div className="flex flex-col">
-                          <div className="mb-4 flex items-center">
+                          <div className="mb-1.5 flex items-center">
                             <input
                               type="checkbox"
                               checked={tvQuotaEnabled}
                               onChange={() => setTvQuotaEnabled((s) => !s)}
                             />
-                            <span className="ml-2 text-gray-300">
+                            <span className="ml-2 font-semibold text-gray-300">
                               {intl.formatMessage(messages.enableOverride)}
                             </span>
                           </div>
@@ -411,7 +411,7 @@ const UserGeneralSettings: React.FC = () => {
                 )}
               <div className="actions">
                 <div className="flex justify-end">
-                  <span className="ml-3 inline-flex rounded-md shadow-sm">
+                  <span className="inline-flex ml-3 rounded-md shadow-sm">
                     <Button
                       buttonType="primary"
                       type="submit"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -163,6 +163,7 @@ CoreApp.getInitialProps = async (initialProps) => {
     movie4kEnabled: false,
     series4kEnabled: false,
     localLogin: true,
+    plexLogin: true,
     region: '',
     originalLanguage: '',
     partialRequestsEnabled: true,
